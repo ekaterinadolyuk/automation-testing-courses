d8acd325ac30d477dbefeb221d758e0b29040069
EkaterinaDolyuk <ek.dolyuk@gmail.com>
2025-03-07 16:18:46 +0100

    homework for lesson #6 - corrected

--- index-hm-06.js
+++ index-hm-06.js
@@ -2,34 +2,36 @@
 function countNumbers(string) {
     if ((string.length % 2) === 0) {
         let sum1 = 0;
-        for (let j = 0; j <= ((string.length - 1) / 2); j++) {
-            sum1 = +(string[j]) + sum1;
-        }
         let sum2 = 0;
-        for (let k = string.length / 2; k < string.length; k++) {
-            sum2 = +(string[k]) + sum2;
+        const half = string.length / 2;
+
+        for (let i = 0; i < half; i++) {
+            sum1 += +string[i];
+            sum2 += +string[i + half];
         }
-        if(sum1 === sum2) {
-            console.log ('да');
+        if (sum1 === sum2) {
+            console.log('да');
         } else {
-            console.log ('нет');
+            console.log('нет');
         }
     } else {
         console.log('Количество символов в строке нечётное!');
     }
 }
 
-countNumbers('11111');
+countNumbers('1111');
 
 // 2. Дано число n=1000 (может быть заданное любое число). Делите его на 2 столько раз, пока результат деления не станет меньше 50 (может быть любое заданное число). Какое число получится? Посчитайте количество итераций, необходимых для этого (итерация - это проход цикла), и запишите его в переменную num.
 function countIterations(largeNumber, smallNumber) {
     let num = 0;
     let remainder = largeNumber;
-    do {
-        remainder = (remainder / 2);
-        num = num + 1;
+    if (remainder >= smallNumber) {
+        do {
+            remainder = (remainder / 2);
+            num = num + 1;
+        }
+        while (remainder >= smallNumber);
     }
-    while (remainder>=smallNumber);
     console.log(num);
 }
 
@@ -39,22 +41,22 @@ countIterations(200, 5);
 // 3. Дан массив arr. Найдите среднее арифметическое его элементов. Проверьте задачу на массиве с элементами 12, 15, 20, 25, 59, 79.
 
 let arr = [12, 15, 20, 25, 59, 79];
-const sum = arr.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-console.log(sum);
+const average = arr.reduce((accumulator, currentValue) => accumulator + currentValue, 0) / arr.length;
+console.log(average);
 
 
 // 4. Напишите функцию, которая вставит данные в массив с заданного места в массиве. Дан массив [1, 2, 3, 4, 5]. Сделайте из него массив [1, 2, 3, 'a', 'b', 'c', 4, 5].
 function addParameters(currentArray, arrayToInsert, index) {
     let arrayPart1 = currentArray.slice(0, index);
     let arrayPart2 = currentArray.slice(index);
-    let returningArray = (arrayPart1.concat(arrayToInsert)).concat(arrayPart2); 
+    let returningArray = (arrayPart1.concat(arrayToInsert)).concat(arrayPart2);
     return returningArray;
 }
 console.log(addParameters([1, 2, 3, 4, 5], ['a', 'b', 'c'], 3));
 // 5. Напишите функцию, которая вставит данные в массив в заданные несколько мест в массиве. Дан массив [1, 2, 3, 4, 5]. Сделайте из него массив [1, 'a', 'b', 2, 3, 4, 'c', 5, 'e'].
 function addParametersSeveralTimes(givenArray, dataToInsert) {
     let newlyCreatedArr = givenArray;
-    for(let i = 0; i < dataToInsert.length; i++) {
+    for (let i = 0; i < dataToInsert.length; i++) {
         newlyCreatedArr = addParameters(newlyCreatedArr, dataToInsert[i].arrayToInsert, dataToInsert[i].index);
     }
     return newlyCreatedArr;
@@ -77,14 +79,14 @@ console.log(addParametersSeveralTimes([1, 2, 3, 4, 5], [
 
 // 6. Дан массив [3, 4, 1, 2, 7, 30, 50]. Отсортируйте его
 let numbers = [3, 4, 1, 2, 7, 30, 50];
-function compareFn(a, b) {
-    if (a < b) {
-      return -1
-    } else if (a > b) {
-      return 1
+function compareFn(firstNumber, secondNumber) {
+    if (firstNumber < secondNumber) {
+        return -1
+    } else if (firstNumber > secondNumber) {
+        return 1
     }
     return 0
-  }
-  
+}
+
 numbers.sort(compareFn);
 console.log(numbers);
